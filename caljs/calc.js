b024e7ee896f45997c8ad9a98a420dcfc612c055
Avivar101 <63792174+Avivar101@users.noreply.github.com>
2020-07-26 20:31:01 +0100

    Update calc.js

--- caljs/calc.js
+++ caljs/calc.js
@@ -1,5 +1,5 @@
 const calculator = ({
-    displayValue: '0',
+    displayValue: 0,
     firstOperand: null,
     operator: null,
     wiatingForSecondOperand: false,
@@ -18,5 +18,48 @@ updateDisplay();
 const keys = document.querySelector('.calcKeys');
 
 keys.addEventListener('click', (event) => {
-    
+    const {target} = event;
+
+    //check if element clicked is a button
+    //if not, exit  from function
+
+    if(!target.matches('button')) {
+        return;
+    }
+
+    if(target.classList.contains('operator')) {
+        console.log('operator', target.value);
+        return;
+    }
+
+    if (target.classList.contains('equalToo')) {
+        console.log('equalToo', target.value);
+        return;
+    }
+
+    if (target.classList.contains('decimal')) {
+        console.log('decimal', target.value);
+        return;
+    }
+
+    if (target.classList.contains('allClear')) {
+        console.log('allClear', target.value);
+        return;
+    }
+    if (target.classList.contains('backSpace')) {
+        console.log('backSpace', target.value);
+        return;
+    }
+
+    inputDigit(target.value);
+    updateDisplay();
+
 })
+
+
+function inputDigit(digit) {
+    const {displayValue} = calculator;
+    //check if displayValue equals zero then replace it with input
+    calculator.displayValue = displayValue === 0 ? digit : displayValue + digit;
+}
+
